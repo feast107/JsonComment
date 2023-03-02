15126636310154c51b0afdff3038ea1373f5e8a4
Feast <1353216996@qq.com>
2023-03-02 18:24:11 +0800

    Update FileGenerateExtension.cs

--- Feast.JsonAnnotation/Extensions/FileGenerateExtension.cs
+++ Feast.JsonAnnotation/Extensions/FileGenerateExtension.cs
@@ -10,44 +10,5 @@ namespace Feast.JsonAnnotation.Extensions
 {
     internal static class FileGenerateExtension
     {
-        public static string GenerateSourceFile<T>(this FileScope<T> scope)
-        {
-            List<NamespaceRegion> namespaces = new();
-            scope.UsingClass.ForEach(x =>
-            {
-                x.Value.ForEach(c =>
-                {
-                    var cr = new ClassRegion()
-                    {
-                        ClassName = c.GetSelfClassName(),
-                        Class = c
-                    };
-                    var nsp = c.GetNamespace();
-                    if (!namespaces.Any(n =>
-                        {
-                            if (!n.Namespace.Equals(nsp)) return false;
-                            if (!n.Classes.Any(p =>
-                                {
-                                    if (!c.IsInnerClassOf(p.Class)) return false;
-                                    p.Classes.Add(cr);
-                                    return true;
-                                }))
-                            {
-                                n.Classes.Add(cr);
-                            };
-                            return true;
-                        }))
-                    {
-                        namespaces.Add(new ()
-                        {
-                            Namespace = nsp , Classes = { cr }
-                        });
-                    }
-                    
-                   
-                });
-            });
-            return new FileRegion() { Namespaces = namespaces , FilePath = ""}.ContentString();
-        }
     }
 }
