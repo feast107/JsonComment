f795924311acbf3a377a9380308e217fb9c4deac
Feast <1353216996@qq.com>
2023-03-02 18:24:30 +0800

    Create JsonAttributeFilter.cs

---
+++ Feast.JsonAnnotation/Filters/JsonAttributeFilter.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Feast.JsonAnnotation.Structs.Code;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace Feast.JsonAnnotation.Filters
+{
+    internal class JsonAttributeFilter : ISyntaxFilter<JsonAttributeFilter> , ISyntaxReceiver
+    {
+        public static JsonAttributeFilter Instance { get; }= new();
+        private JsonAttributeFilter()
+        {
+            CodeRegion<JsonAttributeFilter>.Filter = this;
+        }
+
+
+        public bool QualifiedClass(ClassDeclarationSyntax syntax, FileRegion<JsonAttributeFilter> context)
+        {
+            return true;
+        }
+
+        public bool QualifiedNamespace(BaseNamespaceDeclarationSyntax syntax, FileRegion<JsonAttributeFilter> context)
+        {
+            return true;
+        }
+
+        public void OnVisitSyntaxNode(SyntaxNode syntaxNode)
+        {
+            CodeRegion<JsonAttributeFilter>.Program.AnalyzeSyntax(syntaxNode);
+        }
+
+        public bool Generated => results != null;
+
+        public Dictionary<string, string> Results => results ??= CodeRegion<JsonAttributeFilter>.Program.ContentStrings;
+#nullable enable
+        private Dictionary<string,string>? results ;
+    }
+}
