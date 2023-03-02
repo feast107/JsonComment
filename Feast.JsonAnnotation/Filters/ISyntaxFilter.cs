1696fbd2697915d91bf6d0d211b171d41846a531
Feast <1353216996@qq.com>
2023-03-02 18:24:27 +0800

    Create ISyntaxFilter.cs

---
+++ Feast.JsonAnnotation/Filters/ISyntaxFilter.cs
@@ -0,0 +1,12 @@
+﻿using Feast.JsonAnnotation.Structs.Code;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace Feast.JsonAnnotation.Filters
+{
+    internal interface ISyntaxFilter<TFilter> where TFilter : ISyntaxFilter<TFilter> 
+    {
+        bool QualifiedClass(ClassDeclarationSyntax syntax, FileRegion<TFilter> context);
+
+        bool QualifiedNamespace(BaseNamespaceDeclarationSyntax syntax, FileRegion<TFilter> context);
+    }
+}
