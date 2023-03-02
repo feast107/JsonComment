c3c44dfd2242c88c44314b7044f3f9159936eb4e
Feast <1353216996@qq.com>
2023-03-02 18:24:51 +0800

    Update PropertyRegion.cs

--- Feast.JsonAnnotation/Structs/Code/PropertyRegion.cs
+++ Feast.JsonAnnotation/Structs/Code/PropertyRegion.cs
@@ -1,11 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System;
+using Feast.JsonAnnotation.Filters;
 
 namespace Feast.JsonAnnotation.Structs.Code
 {
-    internal class PropertyRegion : CodeRegion
+    internal class PropertyRegion<TFilter> : CodeRegion<TFilter>
+        where TFilter : ISyntaxFilter<TFilter>
     {
+        public required PropertyDeclarationSyntax Property { get; init; }
+
         public override string ContentString(int tab = 0)
         {
             throw new NotImplementedException();
