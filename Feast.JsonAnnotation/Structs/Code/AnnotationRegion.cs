e853e748fd13eef5b992c181248e4e1c2227eb1b
Feast <1353216996@qq.com>
2023-03-02 18:24:37 +0800

    Update AnnotationRegion.cs

--- Feast.JsonAnnotation/Structs/Code/AnnotationRegion.cs
+++ Feast.JsonAnnotation/Structs/Code/AnnotationRegion.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Feast.JsonAnnotation.Filters;
 
 namespace Feast.JsonAnnotation.Structs.Code
 {
-    internal class AnnotationRegion : CodeRegion
+    internal class AnnotationRegion<TFilter> : CodeRegion<TFilter>
+        where TFilter : ISyntaxFilter<TFilter>
     {
         public List<string> Annotations = new();
         public override string ContentString(int tab = 0)
