4bad5033bc8cb27984a82420e7005c654fa12409
Feast <1353216996@qq.com>
2023-03-02 18:24:48 +0800

    Update NamespaceRegion.cs

--- Feast.JsonAnnotation/Structs/Code/NamespaceRegion.cs
+++ Feast.JsonAnnotation/Structs/Code/NamespaceRegion.cs
@@ -1,31 +1,78 @@
-﻿using System;
+﻿using Feast.JsonAnnotation.Extensions;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using Feast.JsonAnnotation.Filters;
 
 namespace Feast.JsonAnnotation.Structs.Code
 {
-    internal class NamespaceRegion : CodeRegion
+    internal class NamespaceRegion<TFilter> : CodeRegion<TFilter>
+        where TFilter : ISyntaxFilter<TFilter>
     {
-        public required string Namespace { get; init; }
+        public required BaseNamespaceDeclarationSyntax Namespace
+        {
+            get => syntaxInternal;
+            init
+            {
+                syntaxInternal = value;
+                syntaxInternal.Members.ForEach(x =>
+                {
+                    switch (x)
+                    {
+                        case ClassDeclarationSyntax clz:
+                            if (!Filter.QualifiedClass(clz, Context(clz))) break;
+                            Classes.Add(new ()
+                            {
+                                Class = clz,
+                            });
+                            break;
+                        case BaseNamespaceDeclarationSyntax ns:
+                            if (!Filter.QualifiedNamespace(ns, Context(ns))) break;
+                            Namespaces.Add(new ()
+                            {
+                                Namespace = ns
+                            });
+                            break;
+                    };
+                });
+            }
+        }
 
-        public List<ClassRegion> Classes { get; set; } = new();
+        private readonly BaseNamespaceDeclarationSyntax syntaxInternal;
 
-        public List<NamespaceRegion> Namespaces { get; set; } = new();
+        public List<ClassRegion<TFilter>> Classes { get; set; } = new();
+
+        public List<NamespaceRegion<TFilter>> Namespaces { get; set; } = new();
 
         public override string ContentString(int tab = 0)
         {
             var sb = new StringBuilder();
-            sb.AppendLine($"namespace {Namespace} {{");
+            sb.AppendLineWithTab($"namespace {(Namespace.GetNamespace())} {{", tab);
             Classes.ForEach(c =>
             {
-                sb.AppendLine(c.ContentString(DefaultTabCount));
+                sb.AppendLineWithTab(c.ContentString(DefaultTabCount), tab);
             });
             Namespaces.ForEach(n =>
             {
-                sb.AppendLine(n.ContentString(DefaultTabCount));
+                sb.AppendLineWithTab(n.ContentString(DefaultTabCount), tab);
             });
-            sb.AppendLine("}");
+            sb.AppendLineWithTab("}", tab);
             return sb.ToString();
         }
+
+        public bool TryAddNamespace(BaseNamespaceDeclarationSyntax syntax)
+        {
+            if (!Filter.QualifiedNamespace(syntax, Context(syntax))) return false;
+            if (Namespace == syntax) return true;
+            if (syntax.IsDirectInnerNamespaceOf(Namespace) && Namespaces.All(n => n.Namespace != syntax))
+            {
+                Namespaces.Add(new() { Namespace = syntax });
+                return true;
+            }
+            return Namespaces.Any(n => n.TryAddNamespace(syntax));
+        }
     }
+
+   
 }
