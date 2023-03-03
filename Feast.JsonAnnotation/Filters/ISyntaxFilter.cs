b3c5b8cc44a2b34aec15fa6c327d01bb3dda57a3
Feast <1353216996@qq.com>
2023-03-03 13:24:05 +0800

    Update ISyntaxFilter.cs

--- Feast.JsonAnnotation/Filters/ISyntaxFilter.cs
+++ Feast.JsonAnnotation/Filters/ISyntaxFilter.cs
@@ -5,8 +5,29 @@ namespace Feast.JsonAnnotation.Filters
 {
     internal interface ISyntaxFilter<TFilter> where TFilter : ISyntaxFilter<TFilter> 
     {
+        /// <summary>
+        /// 是否需要这个节点
+        /// </summary>
+        /// <param name="syntax"></param>
+        /// <param name="context"></param>
+        /// <returns></returns>
         bool QualifiedClass(ClassDeclarationSyntax syntax, FileRegion<TFilter> context);
-
+        /// <summary>
+        /// 是否需要这个节点
+        /// </summary>
+        /// <param name="syntax"></param>
+        /// <param name="context"></param>
+        /// <returns></returns>
         bool QualifiedNamespace(BaseNamespaceDeclarationSyntax syntax, FileRegion<TFilter> context);
+        /// <summary>
+        /// 载入节点后的操作
+        /// </summary>
+        /// <param name="target"></param>
+        void PostClassDeclaration(ClassRegion<TFilter> target);
+        /// <summary>
+        /// 载入节点后的操作
+        /// </summary>
+        /// <param name="target"></param>
+        void PostNamespaceDeclaration(NamespaceRegion<TFilter> target);
     }
 }
