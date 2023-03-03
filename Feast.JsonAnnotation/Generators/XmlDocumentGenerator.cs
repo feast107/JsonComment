c7dc0c5dc8c7447a3b63d5b58fdf7729188c8a8b
Feast <1353216996@qq.com>
2023-03-03 15:42:04 +0800

    Update

---
+++ Feast.JsonAnnotation/Generators/XmlDocumentGenerator.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Xml;
+using Feast.JsonAnnotation.Filters;
+using Feast.JsonAnnotation.Structs.Code;
+
+namespace Feast.JsonAnnotation.Generators
+{
+    internal class XmlDocumentGenerator
+    {
+        private XmlDocument Document { get; } = new();
+
+        public string Root { get; init; } = nameof(Document);
+
+        public string Namespace { get; init; } = nameof(Namespace);
+
+        public string Class { get; init; } = nameof(Class);
+
+        public static XmlDocumentGenerator FromFile<TFilter>(FileRegion<TFilter> region) 
+            where TFilter : SyntaxFilter<TFilter>
+        {
+            
+            XmlDocumentGenerator ret = new();
+            region.Namespaces.ForEach(x =>
+            {
+                ret.MapNode();
+            });
+        }
+
+        private void MapNode()
+        {
+            Document.CreateElement(Root,"");
+        }
+    }
+}
