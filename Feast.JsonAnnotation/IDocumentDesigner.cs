6251747d666332b102819841ec01bcb733e6edd9
Feast <1353216996@qq.com>
2023-03-05 11:19:10 +0800

    Create IDocumentDesigner.cs

---
+++ Feast.JsonAnnotation/IDocumentDesigner.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace Feast.JsonAnnotation
+{
+    public interface IDocumentDesigner
+    {
+        void OnConfiguring(DocumentConfig config);
+    }
+
+    public class DocumentConfig
+    {
+        public Expression<Func<string>> Path { get; set; }
+    }
+}
