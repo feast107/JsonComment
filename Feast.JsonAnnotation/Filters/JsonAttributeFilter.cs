b4e391fcd8492b5475c75bd4e556c18d5f83603f
Feast <1353216996@qq.com>
2023-03-05 11:19:06 +0800

    Update JsonAttributeFilter.cs

--- Feast.JsonAnnotation/Filters/JsonAttributeFilter.cs
+++ Feast.JsonAnnotation/Filters/JsonAttributeFilter.cs
@@ -1,11 +1,16 @@
-﻿using Feast.JsonAnnotation.Extensions;
+﻿using System;
+using Feast.JsonAnnotation.Extensions;
 using Feast.JsonAnnotation.Structs.Code;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Text;
+using System.Xml;
 using Feast.JsonAnnotation.Generators;
+using Feast.JsonAnnotation.Structs.Doc;
 
 namespace Feast.JsonAnnotation.Filters
 {
@@ -19,36 +24,55 @@ namespace Feast.JsonAnnotation.Filters
 
         private string AttributeName { get; } = nameof(JsonAnnotationAttribute).WithoutAttribute();
         private string AttributeNamespace { get; } = typeof(JsonAnnotationAttribute).Namespace;
-        private string FullAttributeName { get; } = typeof(JsonAnnotationAttribute).FullName.WithoutAttribute();
 
         public override bool QualifiedClass(ClassDeclarationSyntax syntax, FileRegion<JsonAttributeFilter> context)
         {
             if (!syntax.Modifiers.Has(SyntaxKind.PartialKeyword)) return false;
-            return syntax.GetAllAttributeSyntax().Any(x =>
-            {
-                var name = x.GetName();
-                return name == FullAttributeName
-                       || syntax.GetNamespace().StartsWith(AttributeNamespace) && AttributeName == name
-                       || context.UsingNamespaces.Any(n => $"{n}.{name}" == FullAttributeName)
-                       || context.AliasUsingNamespaces.Any(a =>
-                           name.Replace(a.Key, a.Value).WithoutAttribute() == FullAttributeName);
-            });
+            return syntax.GetAllAttributeSyntax().Any(x => 
+                context.CanReferenceTo(syntax, x, AttributeNamespace, AttributeName));
         }
 
         public override void BeforeGenerateDoc(FileRegion<JsonAttributeFilter> target)
         {
-            XmlDocumentGenerator.MapFile(target);
+            var generator = XmlDocumentGenerator.MapFile(target);
+            var dir = Path.GetDirectoryName(target.FilePath);
+            if (generators.TryGetValue(dir!, out var region))
+            {
+                var code = generator.GetGenerateCode("dom", false);
+                region.Item1.Append(code);
+            }
+            else
+            {
+                var code = generator.GetGenerateCode("dom", true);
+                region = new(new(code), generator.Config);
+                generators[dir] = region;
+            }
         }
+
+        private readonly Dictionary<string, Tuple<StringBuilder, XmlGenerationConfig>> generators = new();
+
+        public Dictionary<string, string> Generators =>
+            generators.Where(x=>x.Value.Item1.Length != 0)
+                .ToDictionary(k => k.Key,
+                    v =>
+                    {
+                        var sb = new StringBuilder();
+                        var ret = $@"new {nameof(Action)}(()=>{{
+    var dom = new {typeof(XmlDocument).FullName}();
+{sb.AppendMultipleLineWith(v.Value.Item1.ToString(),"    ")}
+    dom.{nameof(XmlDocument.Save)}(@""{Path.Combine(v.Key, v.Value.Item2.FileName)}"");
+}}).{nameof(Action.Invoke)}();";
+                        return ret;
+                    });
 
         public void OnVisitSyntaxNode(SyntaxNode syntaxNode)
         {
             CodeRegion<JsonAttributeFilter>.Program.AnalyzeSyntax(syntaxNode);
         }
 
-
         public bool Generated => results != null;
 
-        public Dictionary<string, string> Results => results ??= CodeRegion<JsonAttributeFilter>.Program.ContentStrings;
+        public Dictionary<string, string> Codes => results ??= CodeRegion<JsonAttributeFilter>.Program.ContentStrings;
 #nullable enable
         private Dictionary<string, string>? results;
     }
